2078f5c556f5e0d8879a945ac552c5bdadfb0527
Jack Gaark <jack.gaarkeuken@gmail.com>
2023-04-07 17:59:54 -0400

    fxing firefox error ns data aborted

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import anime from "animejs/lib/anime.es.js";
 import inView from "in-view";
 import "./src/js/tilt.js";
-// import "./src/js/index";
+import "./src/js/index";
 import "./src/css/actors.css";
 import "./src/css/style.css";
 import "./src/css/a-tachyons.css";
@@ -16,92 +16,97 @@ import "./src/css/contact.css";
 
 // Grab our header and desktop header
 // Insert the contents of our header into the document one
-const header = document.querySelector(".header");
-const desktopHeader = document.querySelector(".header-desktop");
-desktopHeader.innerHTML = header.innerHTML;
 
-// 1. When header enters th viewport, hide the desktop header (remove visible class)
-// 2. When header leaves, show it (add the visible class)
+function domLoad() {
+  const header = document.querySelector(".header");
+  const desktopHeader = document.querySelector(".header-desktop");
+  desktopHeader.innerHTML = header.innerHTML;
 
-inView(".header")
-  .on("enter", (el) => desktopHeader.classList.remove("visible"))
-  .on("exit", (el) => desktopHeader.classList.add("visible"));
+  // 1. When header enters th viewport, hide the desktop header (remove visible class)
+  // 2. When header leaves, show it (add the visible class)
 
-// Enabling the tilt library on all our images
-VanillaTilt.init(document.querySelectorAll(".image"), {
-  max: 25,
-  speed: 400,
-});
+  inView(".header")
+    .on("enter", (el) => desktopHeader.classList.remove("visible"))
+    .on("exit", (el) => desktopHeader.classList.add("visible"));
 
-inView(".fade")
-  .on("enter", (img) => img.classList.add("visible"))
-  .on("exit", (img) => img.classList.remove("visible"));
-
-// // 1. when we click the .register-button, run a function
-// // 2. grab the .front element and add a class of .slide-up
-// const registerButton = document.querySelector(".register-button");
-// registerButton.addEventListener("click", (event) => {
-//   // stops any default actions from happening
-//   event.preventDefault();
-//   const frontEl = document.querySelector(".front");
-//   frontEl.classList.add("slide-up");
-// });
-
-// //js video control behavior
-
-var bodyEl = document.body,
-  videoWrap = document.querySelector(".video-wrap"),
-  videoEl = videoWrap.querySelector("video"),
-  playCtrl = document.querySelector(".action--play"),
-  closeCtrl = document.querySelector(".action--close");
-
-function init() {
-  console.log("Hello2");
-  playCtrl.addEventListener("click", play);
-  closeCtrl.addEventListener("click", hide);
-  videoEl.addEventListener("canplay", allowPlay);
-  videoEl.addEventListener("ended", hide);
-}
-
-function allowPlay() {
-  console.log("Hello");
-  bodyEl.classList.add("video-loaded");
-}
-
-function play() {
-  videoEl.currentTime = 0;
-  videoWrap.classList.remove("video-wrap--hide");
-
-  videoWrap.classList.add("video-wrap--show");
-  setTimeout(function () {
-    videoEl.play();
-  }, 600);
-}
-
-function hide() {
-  videoWrap.classList.remove("video-wrap--show");
-  videoWrap.classList.add("video-wrap--hide");
-  videoEl.pause();
-}
-
-init();
-
-// grab all the anchor tags on the page
-const anchors = document.querySelectorAll("a");
-// loop over them
-anchors.forEach((anchor) => {
-  // listen for clicks on each one
-  anchor.addEventListener("click", (event) => {
-    // grab the href attribute
-    const href = anchor.getAttribute("href");
-    // if the href starts with a #
-    if (href.charAt(0) === "#") {
-      // stop the default action
-      event.preventDefault();
-      // find the element the href points to and scroll it into view
-      document.querySelector(href).scrollIntoView({
-        behavior: "smooth",
-      });
-    }
+  // Enabling the tilt library on all our images
+  VanillaTilt.init(document.querySelectorAll(".image"), {
+    max: 25,
+    speed: 400,
   });
-});
+
+  inView(".fade")
+    .on("enter", (img) => img.classList.add("visible"))
+    .on("exit", (img) => img.classList.remove("visible"));
+
+  // // 1. when we click the .register-button, run a function
+  // // 2. grab the .front element and add a class of .slide-up
+  // const registerButton = document.querySelector(".register-button");
+  // registerButton.addEventListener("click", (event) => {
+  //   // stops any default actions from happening
+  //   event.preventDefault();
+  //   const frontEl = document.querySelector(".front");
+  //   frontEl.classList.add("slide-up");
+  // });
+
+  // //js video control behavior
+
+  var bodyEl = document.body,
+    videoWrap = document.querySelector(".video-wrap"),
+    videoEl = videoWrap.querySelector("video"),
+    playCtrl = document.querySelector(".action--play"),
+    closeCtrl = document.querySelector(".action--close");
+
+  function init() {
+    console.log("Hello2");
+    playCtrl.addEventListener("click", play);
+    closeCtrl.addEventListener("click", hide);
+    videoEl.addEventListener("canplay", allowPlay);
+    videoEl.addEventListener("ended", hide);
+  }
+
+  function allowPlay() {
+    console.log("Hello");
+    bodyEl.classList.add("video-loaded");
+  }
+
+  function play() {
+    videoEl.currentTime = 0;
+    videoWrap.classList.remove("video-wrap--hide");
+
+    videoWrap.classList.add("video-wrap--show");
+    setTimeout(function () {
+      videoEl.play();
+    }, 600);
+  }
+
+  function hide() {
+    videoWrap.classList.remove("video-wrap--show");
+    videoWrap.classList.add("video-wrap--hide");
+    videoEl.pause();
+  }
+
+  init();
+
+  // grab all the anchor tags on the page
+  const anchors = document.querySelectorAll("a");
+  // loop over them
+  anchors.forEach((anchor) => {
+    // listen for clicks on each one
+    anchor.addEventListener("click", (event) => {
+      // grab the href attribute
+      const href = anchor.getAttribute("href");
+      // if the href starts with a #
+      if (href.charAt(0) === "#") {
+        // stop the default action
+        event.preventDefault();
+        // find the element the href points to and scroll it into view
+        document.querySelector(href).scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+}
+
+window.addEventListener("DOMContentLoaded", domLoad);
